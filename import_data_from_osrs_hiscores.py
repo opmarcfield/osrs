3147cdeaa6b3671948ce1ffc83a1068838114b90
opmarcfield <39411656+opmarcfield@users.noreply.github.com>
2025-11-08 11:56:46 +0200

    Update import_data_from_osrs_hiscores.py

--- import_data_from_osrs_hiscores.py
+++ import_data_from_osrs_hiscores.py
@@ -34,7 +34,7 @@ MINIGAMES = [
     "Giant Mole", "Grotesque Guardians", "Hespori", "Kalphite Queen", 
     "King Black Dragon", "Kraken", "Kree'Arra", "K'ril Tsutsaroth", "Lunar Chests",
     "Mimic", "Nex", "Nightmare", "Phosani's Nightmare", "Obor", 
-    "Phantom Muspah", "Sarachnis", "Scorpia", "Scurrius", "Skotizo", "Sol Heredit", 
+    "Phantom Muspah", "Sarachnis", "Scorpia", "Scurrius", "Shellbane Gryphon", "Skotizo", "Sol Heredit", 
     "Spindel", "Tempoross", "The Gauntlet", "The Corrupted Gauntlet", "The Hueycoatl", 
     "The Leviathan", "The Royal Titans", "The Whisperer", "Theatre of Blood", 
     "Theatre of Blood: Hard Mode", "Thermonuclear Smoke Devil", 
